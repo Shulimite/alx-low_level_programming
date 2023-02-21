eb925a92ad30aeaf3c105f80238d7c3f106e45ab
Shulimite <hosholakeneilwe@gmail.com>
2023-02-20 17:22:13 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printing number using bases
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
