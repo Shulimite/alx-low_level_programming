fa73ec0dc4a3668f2a7951aea48dbb852787f988
Shulimite <hosholakeneilwe@gmail.com>
2023-02-20 16:42:36 -0800

    ecluding some ls

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * ecluding letters
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	 while (a <= 122)
+	 {
+		 if (a == 101 || a == 113)
+		 {
+			 a++;
+			 continue;
+		 }
+		 putchar(a);
+		 a++;
+	 }
+	 putchar('\n');
+	 return (0);
+}
