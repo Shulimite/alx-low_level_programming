49bc955eca4dd6b16978b6bb1428382c61ba79db
Shulimite <hosholakeneilwe@gmail.com>
2023-02-20 17:48:25 -0800

    hexidemal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Numbers and their base
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
