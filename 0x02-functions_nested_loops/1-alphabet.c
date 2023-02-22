6c7d28a3c3ef5ed8cae5ef56287f933f3691b360
Shulimite <hosholakeneilwe@gmail.com>
2023-02-22 12:57:28 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * printing lowercase alphabet
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{ 
+	int a = 97;
+
+	while (a <= 122)
+	{ 
+		_putchar('a');
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
