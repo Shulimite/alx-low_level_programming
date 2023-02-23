2ca3a7aec30dd4375d5f1657878f500d846c5476
Shulimite <hosholakeneilwe@gmail.com>
2023-02-23 11:40:41 -0800

    ten time loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: loop for running ten times
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
