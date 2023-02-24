20af228493910b11d1629d750d7a2f4a4628907b
Shulimite <hosholakeneilwe@gmail.com>
2023-02-24 13:23:05 -0800

    hours per day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 
 	i = 0;
 
-	while (i < 24)
+	while (i < 23)
 	{
 		j = 0;
 
-		while (j < 60)
+		while (j < 59)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
