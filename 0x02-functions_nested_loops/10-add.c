ba3300419a6365480e1c3df08b8c7b3723e9b095
Shulimite <hosholakeneilwe@gmail.com>
2023-02-24 14:34:25 -0800

    sum of ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - sum of two ints
+ * @a - input int 1
+ * @b - input int 2
+ * Return: sum = int a + int b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	 return (c);
+}
