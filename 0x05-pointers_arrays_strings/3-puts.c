67564cdd8521a80d5ab13f5bce918bd316644f39
Shulimite <hosholakeneilwe@gmail.com>
2023-02-27 17:35:10 -0800

    sake

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
-/**
- * _puts - outputing a string
- * @str: Input
- * Return: output str
- */
 
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
 void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		puts(str[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
