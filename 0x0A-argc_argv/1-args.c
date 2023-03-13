e50e3d280af3732123bf897e98e08e2c103c2760
Shulimite <hosholakeneilwe@gmail.com>
2023-03-13 13:05:36 -0700

    passed arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - argument to pointer
+ * @argc: size of argv array and number of command line arguments
+ * @argv: An array of size argc
+ * Return: (0)
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
